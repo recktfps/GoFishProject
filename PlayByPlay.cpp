e5259cfae34db32aba45215c359411c6b512decf
needlequeen <152135534+needlequeen@users.noreply.github.com>
2023-12-11 12:12:14 -0800

    Update PlayByPlay.cpp

--- PlayByPlay.cpp
+++ PlayByPlay.cpp
@@ -8,12 +8,15 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 /*
 PlayByPlay should send every single move to an OutFile. From that OutFile we should be able to show the user a previous game play by play.
 */
-
+   ofstream outFile;
+    outFile.open("Play_By_Play.txt");
 
 PlayByPlay::PlayByPlay(){
+
     
 }
