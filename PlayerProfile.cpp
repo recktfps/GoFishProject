33ffa881c7992cc41abfdb273492a1574c8c1cda
recktfps <104601512+recktfps@users.noreply.github.com>
2023-12-10 18:16:50 -0800

    PlayerProfile keeps track of Wins Losses can possibly implement betting here

--- PlayerProfile.cpp
+++ PlayerProfile.cpp
@@ -1,14 +1,41 @@
-#include "Account.h"
-#include "GoFishGame.h"
-#include "Mode.h"
-#include "PlayByPlay.h"
-#include "Player.h"
 #include "PlayerProfile.h"
-#include "Deck.h"
+
 
 #include <iostream>
 #include <string>
 
-PlayerProfile::PlayerProfile(){
-    
+//Default Constructor with empty name and 0 wins&losses
+PlayerProfile::PlayerProfile() : name(""), wins(0), losses(0) {}
+
+//Constructor for PlayerProfile with player's name
+PlayerProfile::PlayerProfile(const string& Name) : name(Name), wins(0), losses(0) {}
+
+//Setter for the player's name
+void PlayerProfile::setName(const string& Name) {
+    name = Name;
+}
+
+//Getter for player name
+string PlayerProfile::getName() const {
+    return name;
+}
+
+//Increment wins by 1
+void PlayerProfile::incrementWins() {
+    wins++;
+}
+
+//Increment losses by 1
+void PlayerProfile::incrementLosses() {
+    losses++;
+}
+
+//Getter for wins
+int PlayerProfile::getWins() const {
+    return wins;
+}
+
+//Getter for losses
+int PlayerProfile::getLosses() const {
+    return losses;
 }
--- PlayerProfile.h
+++ PlayerProfile.h
@@ -10,21 +10,26 @@
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class PlayerProfile{
     public:
-    PlayerProfile();
+    PlayerProfile(); //Default Constructor
+    PlayerProfile(const string& Name); //Constructor for PlayerProfile with player's name
+
+    void setName(const string& playerName); //Setter for player name
+    string getName() const; //Getter for player name
+
+    void incrementWins(); //Increment wins by 1
+    void incrementLosses(); //Increment losses by 1
+
+    int getWins() const; //Getter for wins
+    int getLosses() const; //Getter for losses
+
+    private:
     string name;
     int wins;
     int losses;
-
-
-
-    private:
-
-
-
-
 };
 #endif
