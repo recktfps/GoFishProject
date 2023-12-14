0c13e2bc36cff3b061f2cadb3a273e4bf3b5764f
needlequeen <152135534+needlequeen@users.noreply.github.com>
2023-12-13 22:26:49 -0800

    Update PlayByPlay.h

--- PlayByPlay.h
+++ PlayByPlay.h
@@ -4,15 +4,23 @@
 #include "Account.h"
 #include "GoFishGame.h"
 #include "Mode.h"
+#include "Card.h"
 #include "Player.h"
 #include "PlayerProfile.h"
 #include "Deck.h"
 
 #include <iostream>
 #include <string>
+
+using namespace std;
 class PlayByPlay{
     public:
-    PlayByPlay();
+    PlayByPlay(); // Constructor
+
+    void PlayerTurn(const string& playerName, string& chosenCard, bool& matchFound);
+    void DrawCard(const string& playerName, Card& drawnCard); // Modify this line
+    void BookFound(const string& playerName);
+    void PrintSummary() const;
 
 
 
