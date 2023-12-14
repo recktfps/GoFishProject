84e3f68c78f748b8d1e2cb86768eab6cbe9a352d
needlequeen <152135534+needlequeen@users.noreply.github.com>
2023-12-13 22:27:23 -0800

    Update PlayByPlay.cpp

--- PlayByPlay.cpp
+++ PlayByPlay.cpp
@@ -16,7 +16,41 @@ PlayByPlay should send every single move to an OutFile. From that OutFile we sho
    //ofstream outFile;
    //outFile.open("Play_By_Play.txt");
 
-PlayByPlay::PlayByPlay(){
+class PlayByPlay {
+public:
+    PlayByPlay(); // Constructor
+    ~PlayByPlay(); // Destructor to close the file
 
-    
+    void PlayerTurn(const string& playerName, string& chosenCard, bool& matchFound);
+    void DrawCard(const string& playerName, Card& drawnCard);
+    // Add a function to log each move
+    void LogMove(const string& playerName, const string& moveDescription);
+
+private:
+    ofstream outFile; // Member variable for ofstream
+};
+
+PlayByPlay::PlayByPlay() {
+    outFile.open("Play_By_Play.txt"); // Open the file in the constructor
+}
+
+PlayByPlay::~PlayByPlay() {
+    outFile.close(); // Close the file in the destructor
+}
+
+void PlayByPlay::PlayerTurn(const string& playerName, string& chosenCard, bool& matchFound) {
+
+    // Log the move
+    LogMove(playerName, "Player's turn");
+}
+
+void PlayByPlay::DrawCard(const string& playerName, Card& drawnCard) {
+
+    // Log the move
+    LogMove(playerName, "Drew a card");
+}
+
+// Function to log each move to the file
+void PlayByPlay::LogMove(const string& playerName, const string& moveDescription) {
+    outFile << playerName << ": " << moveDescription << endl;
 }
